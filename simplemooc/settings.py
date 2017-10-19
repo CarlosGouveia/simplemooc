d640dd1c4009065a684d24427238f5e4a54b9387
CarlosGouveia <carlosadriano_gouveia@hotmail.com>
2017-10-18 23:12:51 -0200

    atualização2

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'simplemooc', 'media')
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
 
 # E-mails
